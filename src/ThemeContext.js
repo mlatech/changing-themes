d6613f8eca38678a604dc8d22d3d93092cf97de3
Karyna Chernyak <mlatech@icloud.com>
2023-03-24 21:38:40 -0500

    themeContext set up

---
+++ src/ThemeContext.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const [theme, setTheme] = React.useState(lightTheme)
+const lightTheme = {
+    backgroundColor: '#FFFFFF',
+    textColor: '#000000',
+    accentColor: '#FCA311'
+  };
+  
+  const darkTheme = {
+    backgroundColor: '#000000',
+    textColor: '#FFFFFF',
+    accentColor: '#FCA311'
+  };
+  
+export default function ThemeContext(){
+    return (
+
+    )
+}
